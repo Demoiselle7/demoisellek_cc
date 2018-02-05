62e1d3f1e9bfa5b53ec1591593e03f5d5078ef8b
Demoiselle7 <yuekewang7@gmail.com>
2018-02-05 13:07:05 +0800

    commit

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const run = (port=3000, host='') => {
 }
 
 if (require.main === module) {
-    const port = 4017
+    const port = 4000
     // host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码
     const host = '0.0.0.0'
     run(port, host)
